051662e96a07987415f17617e77a07f1ef253f12
Guilherme Rv Coelho <guilhermervcoelho@gmail.com>
2015-04-09 17:04:44 -0300

    Fix media query on info page

--- app/pages/info-page/info-page.js
+++ app/pages/info-page/info-page.js
@@ -2,9 +2,18 @@ Polymer('info-page', {
   handleResponse: function(event, detail, sender) {
     this.contact = detail.response;
 
+    this.setScrollHeaderPanelBackground();
+  },
+
+  largeScreenChanged: function() {
+    if(this.contact) {
+      this.setScrollHeaderPanelBackground();
+    }
+  },
+
+  setScrollHeaderPanelBackground: function() {
     if(this.largeScreen) return;
 
     this.$.scrollHeaderPanel.$.headerBg.style.background = 'url(' + this.contact.avatar + ') 0 / cover no-repeat';
-    // this.$.toolbar.style.background = 'url('+this.contact.avatar+') 0 / cover no-repeat';
-  },
+  }
 });
