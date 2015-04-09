4448f639a4f9b6dbfea03d7af9806f3b095c5cdf
Guilherme Rv Coelho <guilhermervcoelho@gmail.com>
2015-04-09 15:55:32 -0300

    Create contacts routes

---
+++ server/modules/contacts/routes.js
@@ -0,0 +1,39 @@
+/**
+ * Module dependencies
+ */
+
+var express = require('express');
+var router = express.Router();
+var contact = require('./controller');
+
+/**
+ * Default callback to the actions
+ *
+ * Sends the resulting data via json
+ */
+
+var callback = function(err, data, res) {
+  if(err) throw err;
+
+  res.json(data);
+};
+
+router.get('/', function(req, res) {
+  contact.retrieve(req, res, callback);
+});
+
+router.get('/:id', function(req, res) {
+  notebook.findOne(req, res, callback);
+});
+
+router.post('/', function(req, res) {
+  contact.create(req, res, callback);
+});
+
+router.put('/:id', function(req, res) {
+  contact.update(req, res, callback);
+});
+
+router.delete('/:id', function(req, res) {
+  contact.delete(req, res, callback);
+});
