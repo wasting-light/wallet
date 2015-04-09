93199cc0f7cc71a80a508640b0f9b2ae9ce26de5
Guilherme Rv Coelho <guilhermervcoelho@gmail.com>
2015-04-09 17:31:08 -0300

    Create input fields on add-page

--- app/pages/add-page/add-page.js
+++ app/pages/add-page/add-page.js
@@ -1,3 +1,11 @@
 Polymer('add-page', {
-
+  created: function() {
+    this.newContact = {
+      name: 'John Doe',
+      avatar: 's3.amazon.com/9u1h23812h39812h',
+      address: 'Sesame Stm 21',
+      email: 'john@doe.com',
+      phone: '555 3211'
+    }
+  }
 });
