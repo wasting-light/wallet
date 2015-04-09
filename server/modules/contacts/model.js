fc9ab7ae96e391fa3f9c784ce5823d568c3f70d8
Guilherme Rv Coelho <guilhermervcoelho@gmail.com>
2015-04-09 15:31:34 -0300

    Create contacts model

---
+++ server/modules/contacts/model.js
@@ -0,0 +1,49 @@
+/**
+ * Module dependencies
+ */
+
+var mongoose = require('mongoose');
+
+var Schema = mongoose.Schema;
+
+/**
+ * Creates the database schema
+ */
+
+var ContactSchema = new Schema({
+  name: {
+    type: String,
+    default: ''
+  },
+
+  phone: {
+    type: String,
+    default: ''
+  },
+
+  email: {
+    type: String,
+    default: ''
+  },
+
+  address: {
+    type: String,
+    default: ''
+  },
+
+  created_at: {
+    type: Date,
+    default: Date.now
+  },
+
+  updated_at: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+/**
+ * Exports the schema
+ */
+
+module.exports = mongoose.model('Contact', ContactSchema);
