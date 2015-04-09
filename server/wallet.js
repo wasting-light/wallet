f11eefefcd1819f372b64ce90709e4db437605de
Guilherme Rv Coelho <guilhermervcoelho@gmail.com>
2015-04-09 14:58:42 -0300

    Create main express file

---
+++ server/wallet.js
@@ -0,0 +1,55 @@
+/**
+ * Module dependencies
+ */
+
+var bodyParser     = require('body-parser');
+var errorHandler   = require('errorhandler');
+var express        = require('express');
+var http           = require('http');
+var methodOverride = require('method-overrride');
+var morgan         = require('morgan');
+var path           = require('path');
+
+/**
+ * Application prototype
+ */
+
+var app = express();
+
+/**
+ * Application configuration
+ */
+
+app.set('port', process.env.PORT || 3000);
+
+/**
+ * Middleware configuration
+ */
+
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.use(methodOverride());
+
+/**
+ * Sets up the public folder
+ */
+
+var publicFolder = path.join(__dirname, '../app');
+app.use(express.static(publicFolder));
+
+/**
+ * Environment configuration
+ */
+
+var env = process.env.NODE_ENV || 'development';
+
+if(env === 'development') {
+  app.use(errorHandler());
+}
+
+/**
+ * Exports the app
+ */
+
+module.exports = app;
