de03015b9ac53f57840c809b58addf99f96924fa
Guilherme Rv Coelho <guilhermervcoelho@gmail.com>
2015-04-09 15:50:13 -0300

    Create contacts controllers

---
+++ server/modules/contacts/controller.js
@@ -0,0 +1,71 @@
+/**
+ * Module dependencies
+ */
+
+var Contact = require('./model');
+
+/**
+ * Creates a new contact
+ */
+
+var create = function(req, res, callback) {
+  var contact = new Contact(req.body);
+
+  contact.save(function(err, data) {
+    callback(err, data, res);
+  });
+};
+
+/**
+ * Retrieves all the contacts
+ */
+
+var retrieve = function(req, res, callback) {
+  var query = {};
+
+  Contact.find(query, function(err, data) {
+    callback(err, data, res);
+  });
+};
+
+/**
+ * Finds one contact by its _id
+ */
+
+var findOne = function(req, res, callback) {
+  var id = req.params.id;
+  var query = {_id: id};
+
+  Contact.findOne(query, function(err, data) {
+    callback(err, data, res);
+  });
+};
+
+/**
+ * Updates one contact by its _id
+ */
+
+var update = function(req, res, callback) {
+  var id = req.params.id;
+  var query = {_id: id};
+  var mod = req.body;
+
+  delete mod._id;
+
+  Contact.update(query, mod, function(err, data) {
+    callback(err, data, res);
+  });
+};
+
+/**
+ * Removes a contact by its _id
+ */
+
+var remove = function(req, res, callback) {
+  var id = req.params.id;
+  var query = {_id: id};
+
+  Contact.remove(query, function(err, data) {
+    callback(err, data, res);
+  });
+};
